8d0fbca4d90314a7a9c47e6cde1cd4745438e736
stnwtr <simon.steinkellner.ss@gmail.com>
2021-07-19 16:18:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gradle Secrets Plugin
+A small plugin which loads an additional properties file for secret values.
---
+++ src/main/kotlin/at/stnwtr/gradle_secrets_plugin/SecretsPlugin.kt
@@ -0,0 +1,10 @@
+package at.stnwtr.gradle_secrets_plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SecretsPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
